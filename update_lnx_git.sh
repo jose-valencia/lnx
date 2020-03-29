f76f1ef12a8669e9b0f8d395fd20d4bf65782cd6
Jose <chps@pop-os.localdomain>
2020-03-29 02:08:32 -0600

    updating update script

--- update_lnx_git.sh
+++ update_lnx_git.sh
@@ -1,36 +1,18 @@
+#!/bin/sh
+[ ! -d "/home/chps/git" ] &&   mkdir ~/git &&   echo "~/git Created.";
+[ ! -d "/home/chps/git/lnx" ] && mkdir ~/git/lnx && echo "~/git/lnx Created."
+[ ! -d "/home/chps/git/lnx/.config" ] && mkdir ~/git/lnx/.config && echo "~/git/lnx/.config Created."
+[ ! -d "/home/chps/git/lnx/.config/i3" ] && mkdir ~/git/lnx/.config/i3 && echo "~/git/lnx/.config/.config Created."
+[ ! -d "/home/chps/git/lnx/.config/ranger" ] && mkdir ~/git/lnx/.config/ranger && echo "~/git/lnx/.config/ranger Created."
 
-if [ ! -d "/home/chps/git" ]; then
-  mkdir ~/git
-  echo "~/git Created."
-fi
-if [ ! -d "/home/chps/git/lnx" ]; then
-  mkdir ~/git/lnx
-  echo "~/git/lnx Created."
-fi
-if [ ! -d "/home/chps/git/lnx/.config" ]; then
-  mkdir ~/git/lnx/.config
-  echo "~/git/lnx/.config Created."
-fi
-if [ ! -d "/home/chps/git/lnx/.config/i3" ]; then
-  mkdir ~/git/lnx/.config/i3
-  echo "~/git/lnx/.config/.config Created."
-fi
-if [ ! -d "/home/chps/git/lnx/.config/ranger" ]; then
-  mkdir ~/git/lnx/.config/ranger
-  echo "~/git/lnx/.config/ranger Created."
-fi
-
-cp /home/chps/.bashrc ~/git/lnx/
-echo ".bachrc Copied."
-cp /home/chps/.Xresources ~/git/lnx/
-echo ".Xresources Copied."
-cp /home/chps/i3blocks.conf ~/git/lnx/
-echo "i3blocks.conf Copied."
-cp /home/chps/.config/compton.conf ~/git/lnx/.config/
-echo "compton.conf Copied." 
-cp -r /home/chps/.config/i3/* ~/git/lnx/.config/i3/
-echo "i3/ Copied." 
-cp -r /home/chps/.config/ranger/* ~/git/lnx/.config/ranger
-echo "ranger/ Copied." 
+cp /home/chps/.bashrc ~/git/lnx/ && echo ".bachrc Copied."
+cp /home/chps/.Xresources ~/git/lnx/ && echo ".Xresources Copied."
+cp /home/chps/i3blocks.conf ~/git/lnx/ && echo "i3blocks.conf Copied."
+cp /home/chps/.config/compton.conf ~/git/lnx/.config/ && echo "compton.conf Copied." 
+cp -r /home/chps/.config/i3/* ~/git/lnx/.config/i3/ && echo "i3/ Copied." 
+cp -r /home/chps/.config/ranger/* ~/git/lnx/.config/ranger && echo "ranger/ Copied." 
 echo ""
 echo "Done."
+
+#git commit -a
+#git push origin master
