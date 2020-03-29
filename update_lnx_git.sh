10406a110a17b5973079d218db1cad8bd503038e
Jose <chps@pop-os.localdomain>
2020-03-29 02:28:03 -0600

    updating messages in update script

--- update_lnx_git.sh
+++ update_lnx_git.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-[ ! -d "/home/chps/git" ] &&   mkdir ~/git &&   echo "~/git Created.";
-[ ! -d "/home/chps/git/lnx" ] && mkdir ~/git/lnx && echo "~/git/lnx Created."
-[ ! -d "/home/chps/git/lnx/.config" ] && mkdir ~/git/lnx/.config && echo "~/git/lnx/.config Created."
-[ ! -d "/home/chps/git/lnx/.config/i3" ] && mkdir ~/git/lnx/.config/i3 && echo "~/git/lnx/.config/.config Created."
-[ ! -d "/home/chps/git/lnx/.config/ranger" ] && mkdir ~/git/lnx/.config/ranger && echo "~/git/lnx/.config/ranger Created."
+[ ! -d "/home/chps/git" ] && (mkdir ~/git && echo "~/git Created." || echo "Error creating ~/git.");
+[ ! -d "/home/chps/git/lnx" ] && (mkdir ~/git/lnx && echo "~/git/lnx Created." || echo "Error creating ~/git/lnx");
+[ ! -d "/home/chps/git/lnx/.config" ] && (mkdir ~/git/lnx/.config && echo "~/git/lnx/.config Created." || echo "Error creating ~/git/lnx/.config"); 
+[ ! -d "/home/chps/git/lnx/.config/i3" ] && (mkdir ~/git/lnx/.config/i3 && echo "~/git/lnx/.config/.i3 Created." || echo "Error creating ~/git/lnx/.config/.i3");
+[ ! -d "/home/chps/git/lnx/.config/ranger" ] && (mkdir ~/git/lnx/.config/ranger && echo "~/git/lnx/.config/ranger Created." || echo "Error creating ~/git/lnx/.config/ranger");
 
 cp /home/chps/.bashrc ~/git/lnx/ && echo ".bachrc Copied."
 cp /home/chps/.Xresources ~/git/lnx/ && echo ".Xresources Copied."
